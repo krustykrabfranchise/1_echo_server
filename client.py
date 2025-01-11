1bf24296e8dcc9d2fb7dd2cb57942c7981f0c552
author <email@email.com>
2025-01-11 13:34:55 +0400

    add solution to part 1

--- client.py
+++ client.py
@@ -1,16 +1,31 @@
 import socket
-from time import sleep
+import threading
 
-sock = socket.socket()
-sock.setblocking(1)
-sock.connect(('10.38.165.12', 9090))
+def receive_messages(client_socket):
+    while True:
+        try:
+            data, _ = client_socket.recvfrom(1024)
+            print(data.decode())
+        except:
+            break
 
-#msg = input()
-msg = "Hi!"
-sock.send(msg.encode())
+def start_client(server_host, server_port):
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    client_socket.setblocking(False)
+    server_address = (server_host, server_port)
 
-data = sock.recv(1024)
+    threading.Thread(target=receive_messages, args=(client_socket,), daemon=True).start()
 
-sock.close()
+    try:
+        while True:
+            message = input()
+            if message.lower() == "exit":
+                client_socket.sendto(message.encode(), server_address)
+                break
+            client_socket.sendto(message.encode(), server_address)
+    finally:
+        client_socket.close()
+        print("Клиент завершил работу")
 
-print(data.decode())
+if __name__ == "__main__":
+    start_client("127.0.0.1", 60000)
