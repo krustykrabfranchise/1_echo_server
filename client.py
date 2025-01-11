2873e28940f0e0047beeba6febbc366dc3795f5e
author <email@email.com>
2025-01-11 13:38:46 +0400

    default state

--- client.py
+++ client.py
@@ -1,31 +1,16 @@
 import socket
-import threading
+from time import sleep
 
-def receive_messages(client_socket):
-    while True:
-        try:
-            data, _ = client_socket.recvfrom(1024)
-            print(data.decode())
-        except:
-            break
+sock = socket.socket()
+sock.setblocking(1)
+sock.connect(('10.38.165.12', 9090))
 
-def start_client(server_host, server_port):
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    client_socket.setblocking(False)
-    server_address = (server_host, server_port)
+#msg = input()
+msg = "Hi!"
+sock.send(msg.encode())
 
-    threading.Thread(target=receive_messages, args=(client_socket,), daemon=True).start()
+data = sock.recv(1024)
 
-    try:
-        while True:
-            message = input()
-            if message.lower() == "exit":
-                client_socket.sendto(message.encode(), server_address)
-                break
-            client_socket.sendto(message.encode(), server_address)
-    finally:
-        client_socket.close()
-        print("Клиент завершил работу")
+sock.close()
 
-if __name__ == "__main__":
-    start_client("127.0.0.1", 60000)
+print(data.decode())
--- server.py
+++ server.py
@@ -1,31 +1,20 @@
 import socket
-import threading
 
-def handle_client(server_socket, clients):
-    while True:
-        data, addr = server_socket.recvfrom(1024)
-        message = data.decode()
-        if addr not in clients:
-            clients.add(addr)
-        if message.lower() == "exit":
-            clients.remove(addr)
-            continue
-        for client in clients:
-            if client != addr:
-                server_socket.sendto(f"[{addr}] {message}".encode(), client)
+sock = socket.socket()
+sock.bind(('', 9090))
+sock.listen(0)
+conn, addr = sock.accept()
+print(addr)
 
-def start_server(host, port):
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    server_socket.bind((host, port))
-    print(f"Сервер запущен на {host}:{port}")
-    clients = set()
-    threading.Thread(target=handle_client, args=(server_socket, clients), daemon=True).start()
-    while True:
-        command = input()
-        if command.lower() == "stop":
-            break
-    server_socket.close()
-    print("Сервер остановлен")
+msg = ''
 
-if __name__ == "__main__":
-    start_server("127.0.0.1", 60000)
+while True:
+	data = conn.recv(1024)
+	if not data:
+		break
+	msg += data.decode()
+	conn.send(data)
+
+print(msg)
+
+conn.close()
